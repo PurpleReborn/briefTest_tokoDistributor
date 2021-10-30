8ce8bdbb3070e5b148c79078c10c1b9cc3bb3b47
SANDI MUHAMAD RIZALUL <79769140+PurpleReborn@users.noreply.github.com>
2021-10-30 10:58:39 +0700

    fix height product

--- src/screens/Product.js
+++ src/screens/Product.js
@@ -144,7 +144,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   itemWrap: {
-    height: Dimensions.get('window').width / 0.7,
+    height: Dimensions.get('window').width / 0.6,
     backgroundColor: '#FADBD8',
     marginTop: 10,
   },
